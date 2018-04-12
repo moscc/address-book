b3e4258d7e3d21f5674c1816444f416976836f24
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-04-12 22:11:49 +0200

    Finished address-book project

---
+++ features/support/world.js
@@ -0,0 +1,72 @@
+const { setWorldConstructor } = require('cucumber')
+const { expect } = require('chai')
+const puppeteer = require('puppeteer')
+
+const HOME_PAGE = 'http://localhost:3000'
+
+class AddressBookWorld {
+  constructor() {}
+
+  async openHomePage() {
+    this.browser = await puppeteer.launch({ headless: false, slowMo: 40 })
+    this.page = await this.browser.newPage()
+    await this.page.goto(HOME_PAGE)
+  }
+
+  async closeHomePage() {
+    await this.browser.close()
+  }
+
+  async pageHasTextContent(expectedContent) {
+    const pageContent = await this.page.content()
+    let match = pageContent.match(expectedContent)
+    let actualContent = match[0]
+
+    expect(actualContent).to.be.eq(expectedContent)
+  }
+
+  async pageDoesNotHaveTextContent(unexpectedContent) {
+    const pageContent = await this.page.content()
+    let actualContent = pageContent.match(unexpectedContent)
+
+    expect(actualContent).to.be.eq(null)
+  }
+
+  async clickOnButton(btnName) {
+    const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
+    await this.page.waitForSelector(btnSelector)
+    await this.page.click(btnSelector)
+  }
+
+  async fillFormField(field, content) {
+    const inputSelector = `#contact-${field}`
+    await this.page.waitForSelector(inputSelector)
+    this.inputElement = await this.page.$(inputSelector)
+    await this.inputElement.type(content)
+  }
+
+  async checkContactStorageCount(expectedCount) {
+    const actualCount = await this.page.evaluate(() => {
+    const contacts = JSON.parse(window.localStorage.getItem('contacts'))
+    return contacts.length
+    })
+
+    expect(actualCount).to.be.eq(expectedCount)
+  }
+
+  btnSelectorFromName(btnName) {
+    switch (btnName) {
+      case 'add contact':
+        return '.add-contact'
+        break
+      case 'save contact':
+        return '.save-contact'
+        break
+      default:
+        throw `${btnName} button is not defined`
+        break
+    }
+  }
+}
+
+setWorldConstructor(AddressBookWorld)
---
+++ src/app.js
@@ -0,0 +1,56 @@
+const storage = window.localStorage
+
+const renderContacts = () => {
+    // Read all the contacts from the storage
+    const contacts = JSON.parse(storage.getItem('contacts'))
+
+let div = document.querySelector('.contact-list')
+
+if(contacts) {
+  div.innerHTML = ''
+  // render the Contacts
+  const ul = document.createElement('ul')
+
+  contacts.forEach(contact => {
+    let li = document.createElement('li')
+
+  li.innerHTML = `
+    <span>${contact.name}</span>
+    <span>${contact.email}</span>
+    <span>${contact.phone}</span>
+    `
+    ul.appendChild(li)
+  })
+
+  div.appendChild(ul)
+} else {
+  div.innerHTML = '<p>You have no contacts in your address book</p>'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderContacts()
+  const contactForm = document.querySelector('.new-contact-form')
+
+  contactForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+  // Reads all the input fields and get their values
+  const { name, email, phone, company, notes, twitter } = contactForm.elements
+
+  const contact = {
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    company: company.value,
+    notes: notes.value,
+    twitter: twitter.value,
+  }
+
+  console.log(contact)
+
+  // Saves input to our storage
+  storage.setItem('contacts', JSON.stringify([contact]))
+  renderContacts()
+  })
+})
